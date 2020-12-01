bd498bb58d1354c0e08df628d1830509025b9131
leightonvanrooijen <leightonvanrooijen@gmail.com>
2020-12-01 16:11:48 +1300

    fixed loading file location

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import WebsiteGrid from "./WebsiteGrid";
 import { makeStyles } from "@material-ui/core/styles";
-import Loading from '../Loading'
+import Loading from './Loading'
 import Ear from "../assets/images/ear.png";
 
 const useStyles = makeStyles({
