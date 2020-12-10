1163a2f53cdf6443d2bfdda933304a96a6cb2db2
leightonvanrooijen <leightonvanrooijen@gmail.com>
2020-12-10 19:58:34 +1300

    Added Google analytics (I hope)

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
 import AppBarProject from "./appbar/AppBarProject";
 import HeaderPhoto from "./appbar/HeaderPhoto";
 import Home from "./Home";
@@ -8,10 +9,10 @@ import ProjectsDisplay from "./projects/ProjectsDisplay";
 import BlogPost from "./blog/BlogPost";
 import Contact from "./Contact";
 
+
 function App() {
   return (
     <div>
-      <Router>
         <AppBarProject />
         <HeaderPhoto />
         <Switch>
@@ -21,7 +22,6 @@ function App() {
           <Route path="/projects" component={ProjectsDisplay} />
           <Route path="/contact" component={Contact} />
         </Switch>
-      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 
-import { ThemeProvider } from '@material-ui/core/styles'
-import theme from './theme'
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
 
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://leightonv.co.nz:81/api',
-  cache: new InMemoryCache()
+  uri: "http://leightonv.co.nz:81/api",
+  cache: new InMemoryCache(),
+});
+
+var history = createBrowserHistory();
+
+ReactGA.initialize('G-26C3YTHET2');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+history.listen((location) => {
+  window.ga('set', 'page', location.pathname + location.search);
+  window.ga('send', 'pageview');
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <ThemeProvider theme={theme}>
-    <App />
+      <Router history={history}>
+        <App />
+      </Router>
     </ThemeProvider>
   </ApolloProvider>,
 
-  document.getElementById('root')
-)
-
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
