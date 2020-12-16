d09129fa470750107b49d1653c0b6af337bf39ca
leightonvanrooijen <leightonvanrooijen@gmail.com>
2020-12-16 17:29:50 +1300

    Changed url to display slug not id of post

--- src/components/blog/BlogTile.js
+++ src/components/blog/BlogTile.js
@@ -55,7 +55,7 @@ export default function MediaCard(props) {
 
   return (
     <Card square={false} className={classes.root}>
-      <CardActionArea component={Link} to={`/blog/${props.entry.id}`}>
+      <CardActionArea component={Link} to={`/blog/${props.entry.slug}`}>
         <Grid container >
           <Grid item xs={12} sm={9}>
             <CardContent className={classes.content}>
