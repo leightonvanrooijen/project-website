c6622066a23093f3e4b94b5c07bdb8976ca9d6c4
leightonvanrooijen <leightonvanrooijen@gmail.com>
2021-01-13 17:04:08 +1300

    added footer to site

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-
 import Routes from "./Routes.js";
 import AppBarProject from "./appbar/AppBarProject";
+import Footer from "./Footer";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "relative",
+    minHeight: "100vh",
+    paddingBottom: "90px",
+  },
+}));
 
 export default function App() {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <AppBarProject />
       <Routes />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: "80px",
+  }
+}));
+
+
+export default function Footer() {
+    const classes = useStyles();
+    
+  return (
+    <div className={classes.root}></div>
+  );
+}
